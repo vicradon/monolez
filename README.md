9d839dc9d95d4fc4a37f6c43ab315320e895b9b9
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2023-01-07 15:59:22 +0000

    chore: add form and excalidraw link

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Monolez
 
 A serverless engine for deploying serverless functions.
+
+## Architecture
+
+https://excalidraw.com/#json=9MNFXbz3Nn62HhKlh8MfD,j7y7lHK60-EWvcqStkiNsA
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,51 @@
-import { Box, Heading } from "@chakra-ui/react";
+import {
+  Box, Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Flex,
+  useToast
+} from "@chakra-ui/react";
 import HomeLayout from "Layout/HomeLayout";
+import {useState} from 'react'
 
 export default function Home() {
+  const toast = useToast()
+  const [loading, setLoading] = useState(false)
+  const handleSubmit =  async (event) => {
+    event.preventDefault()
+    setLoading(true)
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      toast({
+        title: "error",
+        isClosable: true,
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <HomeLayout>
       <Heading>Monolez Frontend</Heading>
+
+      <Box onSubmit={handleSubmit} mt={"1rem"} width={"600px"} as="form">
+        <FormControl mb={"1rem"}>
+          <FormLabel>Docker image</FormLabel>
+          <Input type='text' />
+        </FormControl>
+
+        <Flex justifyContent={"flex-end"}>
+          <Button isLoading={loading} type="submit">Submit</Button>
+        </Flex>
+
+      </Box>
     </HomeLayout>
   );
 }
+
